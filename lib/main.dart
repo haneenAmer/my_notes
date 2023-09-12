77569cd9148fe4025f043c28b25af79f779a822f
eng. Haneen Amer <89270722+haneenAmer@users.noreply.github.com>
2023-09-12 10:38:42 +0300

    initt

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,37 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:my_notes/sign_in_screen.dart';
+import 'package:my_notes/Screens/home_screen.dart';
+import 'package:my_notes/Screens/login_screen.dart';
+import 'package:my_notes/Screens/sign_in_screen.dart';
+import 'firebase_options.dart';
 
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+
+//   await Firebase.initializeApp();
+//   runApp(const MyApp());
+// }
 void main() async {
+  /// do the authintication befor anything else
   WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp();
+  await Firebase.initializeApp().then((_) {
+    print("Firebase initialized successfully.");
+  }).catchError((error) {
+    print("Firebase initialization error: $error");
+  });
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
-    return const SignInScreen();
+    return const MaterialApp(home: HomeScreen());
   }
 }
